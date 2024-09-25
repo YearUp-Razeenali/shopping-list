eb4d86cfe7e014702e45a5af9a1e3358ba782001
Razeenali <razeenali22@gmail.com>
2024-09-25 15:31:20 -0400

    Added additional items to the shopping list

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -5,7 +5,7 @@ public class ShoppingList {
     public static void main(String[] args){
         System.out.println("Shopping List:");
         System.out.println("----------------------------------");
-        System.out.println("apples\nbananas");
+        System.out.println("apples\nbananas\nbread\nchicken\nwater\norange juice");
 
     }
 
